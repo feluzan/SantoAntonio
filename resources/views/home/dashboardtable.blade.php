096fcf139ad47c9ee110e67c25e95e52daaad5f3
feluzan <feluzan@gmail.com>
2020-05-26 11:36:47 -0300

    no message

--- resources/views/home/dashboardtable.blade.php
+++ resources/views/home/dashboardtable.blade.php
@@ -15,7 +15,11 @@
         @foreach($data as $key => $items)
             <tr>
                 <td> {{ $key }} </td>
+                @if(count($items[1])==0)
+                <td> {{ count($items[0]) }} ({{ "--" }})</td>
+                @else
                 <td> {{ count($items[0]) }} ({{ $helper->formatPorcentagem(count($items[0])/count($items[1])) }})</td>
+                @endif
                 <td> {{ count($items[1]) }} </td>
                 <td> {{ $helper->formatCurrencyValue(array_reduce($items[0]->toArray(), function($carry, $item)
                         {
